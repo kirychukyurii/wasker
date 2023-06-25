cfca44ee6576c989520bfed81d46719e4abbc09c
Ubuntu <me@kirychuk.pp.ua>
2023-06-25 12:08:26 +0000

    feat: add request id interceptor

---
+++ internal/server/interceptor/requestid/requestid.go
@@ -0,0 +1,57 @@
+package requestid
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+type key struct{}
+
+// DefaultXRequestIDKey is metadata key name for request ID
+var DefaultXRequestIDKey = "x-request-id"
+
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		var requestID string
+
+		requestID = HandleRequestID(ctx)
+		ctx = context.WithValue(ctx, key{}, requestID)
+
+		return handler(ctx, req)
+	}
+}
+
+func FromContext(ctx context.Context) string {
+	id, ok := ctx.Value(key{}).(string)
+	if !ok {
+		return ""
+	}
+
+	return id
+}
+
+func HandleRequestID(ctx context.Context) string {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return newRequestID()
+	}
+
+	header, ok := md[DefaultXRequestIDKey]
+	if !ok || len(header) == 0 {
+		return newRequestID()
+	}
+
+	requestID := header[0]
+	if requestID == "" {
+		return newRequestID()
+	}
+
+	return requestID
+}
+
+func newRequestID() string {
+	return uuid.New().String()
+}
